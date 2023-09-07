a90cc07c72db11f4e8943b1ceef5f159cb5e87fb
YusufHasan94 <yhpolok@gmail.com>
2023-09-08 01:27:12 +0600

    update chnages

--- task_server/index.js
+++ task_server/index.js
@@ -8,7 +8,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kaasp8g.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,8 +28,6 @@ async function run() {
     const places = database.collection('touringPlaces');
     
     app.get("/places", async(req, res)=>{
-        const category = req.query.category;
-        const query = {category:category};
         const result = await places.find().toArray();
         res.send(result);
     });
@@ -40,6 +37,30 @@ async function run() {
         const query = {category:category};
         const result = await places.find(query).toArray();
         res.send(result);
+    })
+    
+    app.get("/filterPlaces", async(req, res)=>{
+        const price = parseInt(req.query.price);
+        if(!price){
+          console.log("not set");
+        }
+        // const filter = 
+        const result = await places.find({price:{$lte:price}}).toArray();
+        console.log(result);
+        res.send(result);
+    })
+    
+    app.get("/price-range", async(req, res)=>{
+        const minPrice = parseInt(req.query.minPrice);
+        const maxPrice = parseInt(req.query.maxPrice);
+        if(!minPrice || !maxPrice){
+          console.log("not set");
+        }
+        const result = await places.find().toArray();
+                
+        res.send(result.filter(i => {
+          return i.price>=minPrice && i.price<=maxPrice;
+        }));
     })
 
     await client.db("admin").command({ ping: 1 });
